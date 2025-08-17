504ce4568f64144e47cef987be54f13a11891a67
BushHuang <huangkun0917@qq.com>
2025-08-17 20:58:27 +0800

    Init

---
+++ src/main/java/bushhuang/arcaea/ARCAEARESURRECTIONClient.java
@@ -0,0 +1,10 @@
+package bushhuang.arcaea;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ARCAEARESURRECTIONClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/bushhuang/arcaea/ARCAEARESURRECTIONDataGenerator.java
@@ -0,0 +1,11 @@
+package bushhuang.arcaea;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ARCAEARESURRECTIONDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
